e8db782b09058e0a7b032568dfd120e2d09082be
TheVitik <lokplay2003@gmail.com>
2022-08-18 14:27:46 +0300

    Fix comments

--- src/arrays/Arrays.php
+++ src/arrays/Arrays.php
@@ -5,96 +5,70 @@ namespace arrays;
 class Arrays implements ArraysInterface
 {
     /**
-     * The $input variable contains an array of digits
-     * Return an array which will contain the same digits but repetitive by its value
-     * without changing the order.
-     * Example: [1,3,2] => [1,3,3,3,2,2]
-     *
-     * Make sure the next PHPDoc instructions will be added:
-     * @param array $input
-     * @return array
+     * @param int[] $input
+     * @return int[]
      */
     public function repeatArrayValues(array $input): array
     {
-        $array = [];
-        foreach($input as $value){
-            $tempArray = array_fill(0,$value,$value);
-            $array = array_merge($array,$tempArray);
+        $result = [];
+        foreach ($input as $value) {
+            $tempArray = array_fill(0, $value, $value);
+            $result = array_merge($result, $tempArray);
         }
-        return $array;
+
+        return $result;
     }
 
     /**
-     * The $input variable contains an array of digits
-     * Return the lowest unique value or 0 if there is no unique values or array is empty.
-     * Example: [1, 2, 3, 2, 1, 5, 6] => 3
-     *
-     * Make sure the next PHPDoc instructions will be added:
-     * @param array $input
-     * @return int
+     * @param int[] $input
      */
     public function getUniqueValue(array $input): int
     {
-        if(count($input)==0){
+        if (count($input) == 0) {
             return 0;
         }
-        $array = [];
-        foreach (array_count_values($input) as $key=>$value){
-            if($value===1){
-                $array[] = $key;
+        $uniqueValues = [];
+        foreach (array_count_values($input) as $value => $valueCount) {
+            if ($valueCount === 1) {
+                $uniqueValues[] = $value;
             }
         }
-        if(count($array)==0){
+        if (count($uniqueValues) == 0) {
             return 0;
         }
-        return min($array);
+
+        return min($uniqueValues);
     }
 
 
     /**
-     * The $input variable contains an array of arrays
-     * Each sub array has keys: name (contains strings), tags (contains array of strings)
-     * Return the list of names grouped by tags
-     * !!! The 'names' in returned array must be sorted ascending.
-     *
-     * Example:
-     * [
-     *  ['name' => 'potato', 'tags' => ['vegetable', 'yellow']],
-     *  ['name' => 'apple', 'tags' => ['fruit', 'green']],
-     *  ['name' => 'orange', 'tags' => ['fruit', 'yellow']],
+     * @param array $input [
+     *      'name' => string,
+     *      'tags' => [string]
      * ]
-     *
-     * Should be transformed into:
-     * [
-     *  'fruit' => ['apple', 'orange'],
-     *  'green' => ['apple'],
-     *  'vegetable' => ['potato'],
-     *  'yellow' => ['orange', 'potato'],
+     * @return array [
+     *      $key => [string]
      * ]
-     *
-     * Make sure the next PHPDoc instructions will be added:
-     * @param array $input
-     * @return array
      */
     public function groupByTag(array $input): array
     {
         $tags = [];
-        foreach($input as $arr){
-            $tags = array_merge($tags,$arr['tags']);
+        foreach ($input as $arr) {
+            $tags = array_merge($tags, $arr['tags']);
         }
         $tags = array_unique($tags);
-        $array = [];
-        foreach($tags as $tag){
-            $array[$tag] = [];
-            foreach($input as $arr){
-                if(in_array($tag,$arr['tags'])){
-                    $array[$tag][] = $arr['name'];
+        $result = [];
+        foreach ($tags as $tag) {
+            $result[$tag] = [];
+            foreach ($input as $arr) {
+                if (in_array($tag, $arr['tags'])) {
+                    $result[$tag][] = $arr['name'];
                 }
             }
-            sort($array[$tag]);
+            sort($result[$tag]);
 
         }
-        ksort($array);
-        return $array;
+        ksort($result);
+        return $result;
     }
 }
